9edee35f64009aebe367841bfbe54683f4a04730
Bikram Singh Negi <bikramnegi369@gmail.com>
2026-05-04 17:26:29 +0530

    removed max tickets booking limit

--- app/_schemas/booking.schema.ts
+++ app/_schemas/booking.schema.ts
@@ -4,7 +4,7 @@ export const bookingSchema = z.object({
   fullName: z.string().min(2, "Full name is required"),
   email: z.email("Invalid email"),
   mobile: z.string().min(10, "Invalid number"),
-  tickets: z.number().min(1).max(10),
+  tickets: z.number().min(1, "At least one ticket must be booked"),
   ticketType: z.string().min(1),
 });
 
