27a3528662fc3d9152f5a7a9676054ef493c6669
Bikram Singh Negi <bikramnegi369@gmail.com>
2026-05-06 16:17:30 +0530

    festivals added

---
+++ app/_lib/constants/festivals.constants.ts
@@ -0,0 +1,24 @@
+import { CarouselSlideData } from "@/_types/Carousel.types";
+
+export const famousFestivalsCarousel: CarouselSlideData[] = [
+  {
+    id: 1,
+    src: "",
+    alt: "",
+  },
+  {
+    id: 2,
+    src: "",
+    alt: "",
+  },
+  {
+    id: 3,
+    src: "",
+    alt: "",
+  },
+  {
+    id: 4,
+    src: "",
+    alt: "",
+  },
+];
