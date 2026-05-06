b2f2c3dc1c2961e1af140977f55b9165cbc57e43
Bikram Singh Negi <bikramnegi369@gmail.com>
2026-05-06 17:28:20 +0530

    Added festivals feature

---
+++ app/_components/sections/Marketing/festivals/festivalSection.tsx
@@ -0,0 +1,67 @@
+import { cinzel } from "@/_lib/fonts";
+import { FestivalSection } from "@/_types/festivals.types";
+
+export default function festivalSection({
+  title,
+  description,
+  images,
+  descriptionAnchor = "left",
+}: FestivalSection) {
+  return (
+    <section className="py-5" aria-labelledby="festival-heading">
+      <div className="hidden lg:flex flex-col lg:flex-row gap-8 lg:gap-16">
+        {descriptionAnchor === "left" ? (
+          <>
+            <div className="space-y-4">
+              <h2
+                id="festival-heading"
+                className={`${cinzel.className} text-amber-500 text-2xl font-bold leading-snug`}
+              >
+                {title}
+              </h2>
+              <p className="text-stone-600 text-sm sm:text-base leading-relaxed">
+                {description}
+              </p>
+            </div>
+            {images.length > 0 && (
+              <OverlappingImages images={images} anchor="left" />
+            )}
+          </>
+        ) : (
+          <>
+            {images.length > 0 && (
+              <OverlappingImages images={images} anchor="left" />
+            )}
+            <div className="space-y-4">
+              <h2
+                id="festival-heading"
+                className={`${cinzel.className} text-amber-500 text-2xl font-bold leading-snug`}
+              >
+                {title}
+              </h2>
+              <p className="text-stone-600 text-sm sm:text-base leading-relaxed">
+                {description}
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+      <div className="lg:hidden flex flex-col gap-16 ">
+        <div className="space-y-4 text-center lg:text-start">
+          <h2
+            id="festival-heading"
+            className={`${cinzel.className} text-amber-500 text-2xl font-bold leading-snug `}
+          >
+            {title}
+          </h2>
+          <p className="text-stone-600 text-sm sm:text-base leading-relaxed">
+            {description}
+          </p>
+        </div>
+        {images.length > 0 && (
+          <OverlappingImages images={images} anchor="left" />
+        )}
+      </div>
+    </section>
+  );
+}
---
+++ app/_types/festivals.types.ts
@@ -0,0 +1,6 @@
+export type FestivalSection = {
+  title: string;
+  description: string;
+  images: string[];
+  descriptionAnchor?: "left" | "right"; // default to "left"
+};
