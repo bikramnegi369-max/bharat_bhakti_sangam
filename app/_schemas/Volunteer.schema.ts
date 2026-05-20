8dc987698fee3c1b16196c33ad411647b10ab1f2
Bikram Singh Negi <bikramnegi369@gmail.com>
2026-05-20 15:14:41 +0530

    fixed schema to have only right info such as full name no numbers and phone to be no characters

--- app/_schemas/Volunteer.schema.ts
+++ app/_schemas/Volunteer.schema.ts
@@ -1,10 +1,16 @@
 import z from "zod";
 
 export const VolunteerSchema = z.object({
-  name: z.string().min(1, "Volunteer name is required"),
+  name: z
+    .string()
+    .min(1, "Volunteer name is required")
+    .regex(/^[^0-9]*$/, "Volunteer name should not contain numbers"),
   role: z.string().min(1, "Role is required"),
   email: z.string().email("Enter a valid email"),
-  contact: z.string().min(1, "Contact number is required"),
+  contact: z
+    .string()
+    .min(1, "Contact number is required")
+    .regex(/^[0-9]+$/, "Contact number must contain only digits"),
   profilePicture: z.string().min(1, "Profile picture is required"),
 });
 
